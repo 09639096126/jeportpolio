12aee5a6d7988cee63b5ae4aa00e709b8588ff74
jeraldtulibao <jeraldtulibao@gmail.com>
2024-03-25 15:06:07 +0800

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+export const environment = {
+  production: false,
+
+  // socialLinks
+
+  fbLink: 'https://www.facebook.com/login',
+  instaLink: 'https://www.instagram.com/login',
+  youtubeLink: 'https://www.youtube.com',
+  linkedinLink: 'https://www.linkedin.com',
+  githubLink: 'https://www.github.com',
+
+  // email js
+
+  // create email js acccount
+
+  mailService: 'service_mk9mmws',
+  templateId: 'template_pvvaxvf',
+  emailJSKey: 'G5hXul6GvkQYGjV3i',
+
+  // project
+
+  projectOne: 'https://www.flipkart.com/',
+  projectTwo: 'https://www.flipkart.com/',
+  projectThree:
+    'https://www.flipkart.com/',
+};
